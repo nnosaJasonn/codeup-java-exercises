354d267bf903ffec203683848a3de273434bf005
lindsey.jason.andrew@gmail.com <jasonlindsey@Jasons-MacBook-Pro.local>
2019-06-10 13:32:29 -0500

    string exercise

--- src/StringExercise.java
+++ src/StringExercise.java
@@ -1,11 +1,27 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class StringExercise {
     public static void main(String[] args) {
        /* String message = "We don't need no education \n We don't need no thought control \n Check \"this\" out!, \"s inside of \"s! \nIn windows, the main drive is usually C:\\ \n I can do backslashes \\, double backslashes \\\\, and the amazing triple backslash \\\\\\";
         System.out.println(message);*/
 
-       String string = "hello, my name is mike";
+     /*  String string = "hello, my name is mike";
         System.out.println(string);
-        System.out.println(flipString(string));
+        System.out.println(flipString(string));*/
+
+        String[] names = {"Rick", "Stacey", "Brad", "Becca"};
+        String x;
+        try {
+            x = names[100];
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Exception caught!");
+            x = "";
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            System.out.println("This will always run.");
+        }
     }
     public static String flipString(String myStr) {
         if (myStr.isEmpty()){
